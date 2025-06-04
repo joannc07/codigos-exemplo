2da0574d02c6e90d8fc43d5c0aa355e574a1739c
Joan Nassr Conte <138223300+joannc07@users.noreply.github.com>
2025-06-03 23:50:32 -0300

    Update poo.java

--- poo.java
+++ poo.java
@@ -1,4 +1,4 @@
-// Classe principal
+
 public class Main {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa("João", 25);
@@ -6,18 +6,18 @@ public class Main {
     }
 }
 
-// Classe Pessoa
+
 class Pessoa {
     private String nome;
     private int idade;
 
-    // Construtor
+    
     public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
 
-    // Método
+    
     public void mostrarDados() {
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
